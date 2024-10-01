317f0b19514b6cf80b0b121ecc1163de2d2367dd
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2024-10-01 09:57:25 +0200

    Bump plugin version to 2.2.1

--- immich-plugin.lrplugin/Info.lua
+++ immich-plugin.lrplugin/Info.lua
@@ -26,6 +26,6 @@ return {
 
 	LrPluginInfoURL = 'https://github.com/bmachek/lrc-immich-plugin',
 
-	VERSION = { major = 2, minor = 2, revision = 0, build = "", },
+	VERSION = { major = 2, minor = 2, revision = 1, build = "", },
 
 }
