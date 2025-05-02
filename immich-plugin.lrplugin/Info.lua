38b3f077c01acfe9c65966dd5ae0d6b37522529d
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2025-05-02 15:31:05 +0200

    Bump plugin version to 3.0.1.

--- immich-plugin.lrplugin/Info.lua
+++ immich-plugin.lrplugin/Info.lua
@@ -48,6 +48,6 @@ return {
 
 	LrPluginInfoURL = 'https://blog.fokuspunk.de/lrc-immich-plugin/',
 
-	VERSION = { major = 3, minor = 0, revision = 0, build = 0, },
+	VERSION = { major = 3, minor = 0, revision = 1, build = 0, },
 
 }
