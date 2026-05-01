228df25df1f336498493e2dceefcf3e3df824429
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2026-05-01 21:35:11 +0200

    Bump version to 4.1.1

--- immich-plugin.lrplugin/Info.lua
+++ immich-plugin.lrplugin/Info.lua
@@ -48,6 +48,6 @@ return {
 
 	LrPluginInfoURL = 'https://github.com/bmachek/lrc-immich-plugin/',
 
-	VERSION = { major = 4, minor = 1, revision = 0, build = 0, },
+	VERSION = { major = 4, minor = 1, revision = 1, build = 0, },
 
 }
